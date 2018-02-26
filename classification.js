95b8f79b7e0c6f5a8c6134d695c7eaff0ee496bb
milliampere <millla@hotmail.com>
2018-02-26 14:49:19 +0100

    New classification array.

--- classification.js
+++ classification.js
@@ -10,41 +10,39 @@ fs.readFile( './classification.xml', (err, data) => {
         const foodsYay = result.LivsmedelsLista.Livsmedel;
 
         const array = []; //array with all classification numbers that we want to use
-        const array2 = []; //just used to make codeArray
         const arrayItem = []; //array with number of all products that we can use to compare and reduce our nutrition db
+        const arrayOfGroups = [];
 
         const foods = foodsYay.map((food, i) => {
 
-            const code = food['Klassificeringar'][0]['Klassificering'][0]['Varden'][0]['KlassificeringVarde'][0]['Kod'][0]
-            const name = food['Klassificeringar'][0]['Klassificering'][0]['Varden'][0]['KlassificeringVarde'][0]['Namn'][0]
-            const number = food['Nummer'][0];
+            const name = food['Namn'][0];
+            const group = food['Huvudgrupp'][0];
+            const euroFirName = food['Klassificeringar'][0]['Klassificering'][0]['Varden'][0]['KlassificeringVarde'][0]['Namn'][0];
+            const euroFirCode = food['Klassificeringar'][0]['Klassificering'][0]['Varden'][0]['KlassificeringVarde'][0]['Kod'][0]
+            const livsmedelsverketId = food['Nummer'][0];
 
-            //console.log(name, number, code);
-
-            //array with all the classificationcodes that we dont want to include, example fastfood.
-            const codeArray = ['A0866' , 'A0862' , 'A0810' , 'A0310' , 'A0312' , 'A0818' , 'A0819' , 'A0820' , 'A0830' , 'A0799' , 'A0804' , 'A0865' , 'A0828' , 'A0861' , 'A0832' , 'A0842' , 'A0822' , 'A0792' , 'A0821' , 'A0797' , 'A0868' , 'A0838' , 'A0789' , 'A0863' , 'A0843' , 'A0870' , 'A0847' , 'A0848' , 'A0849' , 'A0850']
-            if(!codeArray.includes(code)){
-                arrayItem.push(number);
+            //array with all the euroFir classification codes that we dont want to include, example fastfood.
+            const codeArray = ['A0866' , 'A0862' , 'A0810' , 'A0310' , 'A0312' , 'A0864', 'A0818' , 'A0819' , 'A0820' , 'A0830' , 'A0799' , 'A0804' , 'A0865' , 'A0828' , 'A0861' , 'A0832' , 'A0842' , 'A0822' , 'A0792' , 'A0821' , 'A0797' , 'A0868' , 'A0838' , 'A0789' , 'A0863' , 'A0843' , 'A0870' , 'A0847' , 'A0848' , 'A0849' , 'A0850']
+            if(!codeArray.includes(euroFirCode)){
+                array.push({livsmedelsverketId, name, group, euroFirName, euroFirCode})
             }
 
-            if(codeArray.includes(code)){
-                return
-            } else if (!array.includes(code)){
-                array.push(code);
-                array2.push(
-                {
-                    code,
-                    name,
-                }
-            )}
+            // make array of groups
+            if(!arrayOfGroups.includes(group)){
+                arrayOfGroups.push(group);
+            }
+
         });
 
-        console.log(arrayItem.length);
+        console.log(array);
 
+        // print array of groups
+        console.log(arrayOfGroups);
+        
         /* Save foods to db.json fil */
-        fs.writeFile('numberList.json', JSON.stringify(arrayItem), (err) => {
+         fs.writeFile('classificationList.json', JSON.stringify(array), (err) => {
             if (err) throw err;
             console.log('The file has been saved!');
-        });
+        }); 
     });
  });
